f153c2278583874ec66e0225308c0eaf18afc49a
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2023-12-09 11:45:22 +0900

    initialize test

---
+++ test/test.ts
@@ -0,0 +1,32 @@
+import { expect } from "chai";
+import { ethers, upgrades } from "hardhat";
+
+describe("Contract Test", () => {
+  let owner: any;
+  let addr1: any;
+  let addr2: any;
+  let oracle: any;
+  let provider: any;
+  let contract: any;
+
+  async function deployContract() {
+    const ContractFactory = await ethers.getContractFactory("PredictionMarket");
+    const contract = await upgrades.deployProxy(ContractFactory, [
+      oracle.address,
+      "This is prediction market for governance",
+      "YES",
+      "NO",
+    ]);
+    return contract;
+  }
+
+  beforeEach(async () => {
+    [owner, addr1, addr2, oracle] = await ethers.getSigners();
+    contract = await deployContract();
+    provider = ethers.provider;
+  });
+
+  it("Should deploy the contract", async () => {
+    expect(contract.address).to.not.equal(0);
+  });
+});
